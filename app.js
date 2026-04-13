226ac0c586ca5cfa35d8c2c23138f28358254f5a
LUIS FERNANDO CRISTERNA ARAMBURO <2024030374@upsin.edu.mx>
2026-04-13 02:19:35 -0700

    Correccion de app js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import router from './router/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const puerto = 3006;
 
@@ -9,7 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../unidad0353-front')));
 
 app.use('/', router);
 
